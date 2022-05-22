99ee1b66342aaa3b2dc4fc8aa4d673c8cd1ce6cd
Juan <juand.montoyam@uqvirtual.edu.co>
2022-05-21 23:46:35 -0500

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+<b-card no-body>
+      <b-row>
+        <b-col cols="12" md="8">
+            <img src="../assets/tablero.png" alt="">
+        </b-col>
+        <b-col cols="12" md="4">
+            <img src="../assets/logo.png" alt="">
+        </b-col>
+        
+      </b-row>
+</b-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
