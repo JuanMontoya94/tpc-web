f2a9675799789f2a99d59393f0602b11db0df5dc
Juan <juand.montoyam@uqvirtual.edu.co>
2022-05-22 16:10:43 -0500

    movimientos

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,17 +1,22 @@
 <template>
+  <div class="card card-info">
+  
 <b-card no-body>
       <b-row>
         <b-col cols="12" md="8">
-            <img src="../assets/tablero.png" alt="">
+            <img src="../assets/pregunta1.png" class="card-img-top" alt="pregunta1" width=700px>
         </b-col>
         <b-col cols="12" md="4">
-            <img src="../assets/logo.png" alt="">
+            <img src="../assets/arriba.png" alt="movimiento1" width=100px>
+            <img src="../assets/abajo.png" alt="movimiento1" width=100px>
+            <img src="../assets/izquierda.png" alt="movimiento1" width=100px>
+            <img src="../assets/derecha.png" alt="movimiento1" width=100px>
         </b-col>
         
       </b-row>
 </b-card>
+</div>
 </template>
-
 <script>
 export default {
 
